1e7b224b80fea169668b8f146a7d5f91a025c54a
XAVIER Clément Antoine <clement-antoine.xavier@epitech.eu>
2025-05-16 16:46:25 +0400

    Feat: Add RadixUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
 export const metadata: Metadata = {
   title: "Silver Octo Winner",
@@ -14,7 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <Theme>
+          {children}
+          <ThemePanel/>
+        </Theme>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+'use client';
+
+import { Flex } from "@radix-ui/themes";
+
 export default function Page() {
   return (
-    <></>
+    <Flex direction="column" gap="2">
+    </Flex>
   );
 }
