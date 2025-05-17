fb0f1a5d967ac7c30b3c75abbe28f16bdb0e3763
Nathan Maillot <nathan.maillot@epitech.eu>
2025-05-17 13:20:31 +0400

    feat(editor-nav): add first version of editor head nav

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import HeadNav from "@/components/editor/HeadNav";
+import { Grid } from "@radix-ui/themes";
+
+export default function Test(){
+    return (
+        <div className="flex flex-col gap-2">
+            <HeadNav />
+        </div>
+    )
+}
---
+++ src/components/editor/HeadNav.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { useState } from "react";
+
+import { IconButton, Heading, Avatar, Flex, Theme } from "@radix-ui/themes";
+
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+
+interface IAvatar {
+  name: string;
+  image: string;
+}
+
+export default function HeadNav() {
+  const [title, setTitle] = useState<string>("Untitled");
+  const [avatar, setAvatar] = useState<IAvatar>({
+    name: "John Doe",
+    image: "https://example.com/avatar.jpg",
+  });
+
+  return (
+    <Theme radius="full">
+    <Flex
+      display="flex"
+      justify="between"
+      align="center"
+      gap="2"
+      m="4"
+      mt="0"
+      p="4"
+      pr="8"
+      pl="8"
+      position="absolute"
+      width="80%"
+      top="0"
+      left="50%"
+      className="shadow rounded-b-4xl translate-x-[-50%]"
+    >
+        <span>
+          <IconButton variant="ghost" size="4">
+            <HamburgerMenuIcon width="22" height="22" />
+          </IconButton>
+        </span>
+
+        <span>
+          <Heading as="h1" align="center" size="6">
+            {title}
+          </Heading>
+        </span>
+        <span>
+          <IconButton variant="ghost" size="4">
+            <Avatar
+              src={avatar.image}
+              fallback={_avatarFallback(avatar.name)}
+              size="3"
+            />
+          </IconButton>
+        </span>
+      </Flex>
+    </Theme>
+  );
+}
+
+//////////////////////
+// Private funtions //
+//////////////////////
+
+function _avatarFallback(name: string) {
+  const initials = name
+    .split(" ")
+    .map((word) => word.charAt(0))
+    .join("");
+  return initials;
+}
